d2bc76c360c65469f9a3204a0b23478444b9f540
bixiou <fabre.adrien@hotmail.fr>
2019-03-22 15:12:25 +0100

    good RData

--- code/packages_functions.R
+++ code/packages_functions.R
@@ -39,7 +39,7 @@ package("reshape2")
 package("margins")
 package("rddtools")
 package("rddapp")
-package("mets")
+# package("mets")
 package("plyr")
 # package("ergm") 
 # package("doMC") # for parallel computing, does not work on Windows
